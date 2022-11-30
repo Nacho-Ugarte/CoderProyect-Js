5a95e9abffd49f431bed8f0c45eeb9f380aae4a4
Nacho-Ugarte <igna.ugarte@icloud.com>
2022-11-30 13:31:00 -0300

    Commit de todo

---
+++ src/stock.js
@@ -0,0 +1,64 @@
+const productos =
+[
+    {
+        id: 1,
+        nombre: "Burguer simple",
+        tipo: "hamburguesa",
+        desc: "La infalible, mata la gula",
+        precio: 1200,
+        img: './public/images/simple.jpg',
+        cantidad:1
+    },
+
+    {
+        id: 2,
+        nombre: "Papas simples",
+        tipo: "papas",
+        desc: "La mejor compañia para todo",
+        precio: 500,
+        img: './public/images/papitas.jpg',
+        cantidad:1
+    },
+
+    {
+        id: 3,
+        nombre: "Gaseosa",
+        tipo: "bebida",
+        desc: "Infaltable en dias calurosos",
+        precio: 500,
+        img: './public/images/gaseosa.jpg',
+        cantidad:1
+    },
+
+    {
+        id: 4,
+        nombre: " porcion de helado",
+        tipo: "postre",
+        desc: "El mejor final posible",
+        precio: 1000,
+        img: './public/images/helado.jpg',
+        cantidad:1
+    },
+
+    {
+        id: 5,
+        nombre: "Burga doble",
+        tipo: "hamburguesa",
+        desc: "Para los que siempre van por más",
+        precio: 1400,
+        img: './public/images/doble.jpg',
+        cantidad:1
+    },
+
+    {
+        id: 6,
+        nombre: "Papas cheddar y bacon",
+        tipo: "papas",
+        desc: "Mejorando lo inmejorable",
+        precio: 600,
+        img: './public/images/papascheddar.jpg',
+        cantidad:1
+    }
+]
+
+export { productos };
